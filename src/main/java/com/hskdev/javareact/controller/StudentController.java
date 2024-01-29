ee987c5c6e5b042c42fe224b64157a14f34171f7
Hassan <kkromah12@gmail.com>
2024-01-30 02:27:08 +0300

    Java Spring boot and React frontend Project

--- src/main/java/com/hskdev/javareact/controller/StudentController.java
+++ src/main/java/com/hskdev/javareact/controller/StudentController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin("http://localhost:5173")
 @RestController
 @RequestMapping("/students")
 @RequiredArgsConstructor  // will inject any field that is final
